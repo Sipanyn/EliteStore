ec3c76619d58d77fd3f925253c788b777c900ac3
sipan younan <sipannyounan@gmail.com>
2025-08-30 12:13:19 +0330

    vercel

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import filterMenuHandler from "../utils/FilterMenuHandler";
+import filterMenuHandler from "../utils/filterMenuHandler";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 
